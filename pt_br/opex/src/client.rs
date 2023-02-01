dcce97ac0f10d23078e1858a65113637929929f8
Jansen <jsd.alcantara@gmail.com>
2023-01-31 23:31:23 -0400

    remove logs + edit chapter_page test

--- pt_br/opex/src/client.rs
+++ pt_br/opex/src/client.rs
@@ -12,7 +12,6 @@ const ACCEPT_LANGUAGE_HEADER: &str = "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0
 
 pub async fn opex_html_page(page_url: &str) -> Result<String> {
     let url = format!("{}{}", BASE_URL, page_url);
-    println!("{url}");
     let mut headers = HeaderMap::new();
     headers.insert(header::ACCEPT, ACCEPT_HEADER.parse()?);
     headers.insert(header::REFERER, REFERER_HEADER.parse()?);
@@ -40,11 +39,10 @@ mod tests {
 
     #[tokio::test]
     async fn chapter_page() {
-        let page = opex_html_page("/mangas/leitor/1047").await;
+        let page = opex_html_page("/mangas/leitor/2").await;
         assert!(page.is_ok());
 
         let page = page.unwrap();
-        println!("{}", page);
         assert!(!page.contains("<h2>Erro 404</h2>"));
         assert!(page.contains("paginasLista = \""));
     }
